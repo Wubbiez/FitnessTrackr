63e61b11c0049bd2146c2bf250a5ff846c818e86
Alan Dean <adean3032@gmail.com>
2023-02-01 20:08:45 -0500

    First draft users.js

--- db/users.js
+++ db/users.js
@@ -4,19 +4,40 @@ const client = require("./client");
 
 // user functions
 async function createUser({ username, password }) {
-  
+  try {
+    await client.query(`
+      INSERT INTO users (username, password) VALUES($1, $2)
+      ON CONFLICT (username) DO NOTHING;
+      `, [username, password]);
+  } catch (error) {
+    console.error();
+    throw error;
+  }
+
+
 }
 
 async function getUser({ username, password }) {
-
+  await client.query(`
+    SELECT * FROM users
+    WHERE username = ${username} AND password = ${password};
+  `)
 }
 
 async function getUserById(userId) {
-
+  const { rows: [user] } = await client.query(`
+    SELECT id, username FROM users
+    WHERE id = ${userId};
+  `);
+  return user;
 }
 
 async function getUserByUsername(userName) {
-
+  const { rows: [user] } = await client.query(`
+    SELECT id, username FROM users
+    WHERE username = ${userName};
+  `);
+  return user;
 }
 
 module.exports = {
