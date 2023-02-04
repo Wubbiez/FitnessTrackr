7fc372c579acd5f238ff50f498f72b2df736810e
Zachary Collie <zachc4@vt.edu>
2023-02-04 14:45:32 -0500

    Routines updated

--- db/routine_activities.js
+++ db/routine_activities.js
@@ -1,22 +1,4 @@
 const client = require("./client");
-// async function addActivityToRoutine({
-//     routineId,
-//     activityId,
-//     count,
-//     duration,
-// }) {
-//     try {
-//         const {rows: [routineActivity]} = await client.query(`
-//             INSERT INTO routine_activities("routineId", "activityId", count, duration)
-//             VALUES ($1, $2, $3, $4) ON CONFLICT ("routineId", "activityId") DO NOTHING
-//         RETURNING *;
-//         `, [routineId, activityId, count, duration]);
-//         return routineActivity;
-//     } catch (e) {
-//         console.error(e);
-//         throw e;
-//     }
-// }
 async function addActivityToRoutine({
     routineId,
     activityId,
