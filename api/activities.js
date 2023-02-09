f0ca06acb3d269dcb58fc7d4170f3c09bcfd7bcf
Zachary Collie <zachc4@vt.edu>
2023-02-09 10:55:46 -0500

    GET /activityId/routines passed!

--- api/activities.js
+++ api/activities.js
@@ -1,17 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {createActivity, getAllActivities, getActivityById, getActivityByName} = require('../db/activities');
-const {getPublicRoutinesByActivity} = require('../db/routines');
+const {
+  createActivity,
+  getAllActivities,
+  getActivityById,
+  getActivityByName,
+} = require("../db/activities");
+const { getPublicRoutinesByActivity } = require("../db/routines");
 
 // GET /api/activities/:activityId/routines
-router.get('/:activityId/routines', async (req, res, next) => {
-    try {
-        const {activityId} = req.params;
-        const routines = await getPublicRoutinesByActivity({id: activityId});
-        res.send(routines);
-    } catch (e) {
-        next(e);
-    }
+router.get("/:activityId/routines", async (req, res, next) => {
+  const { activityId } = req.params;
+  try {
+    const routines = await getPublicRoutinesByActivity({ id: activityId });
+    res.send(routines);
+  } catch (e) {
+    next({
+      error: "error",
+      name: "attach error",
+      message: `Activity ${activityId} not found`,
+    });
+  }
 });
 
 // GET /api/activities
