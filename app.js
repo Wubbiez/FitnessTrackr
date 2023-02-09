9fd5b58432940f1b384dc84ff9d00d4c62f23330
Zachary Collie <zachc4@vt.edu>
2023-02-08 20:37:21 -0500

    API Health Passed and Client connected!

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
-require("dotenv").config()
-const express = require("express")
-const app = express()
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const app = express();
+const apiRouter = require("./api");
+
+// connect to DB
+const client = require("./db/client");
+client.connect();
 
 // Setup your Middleware and API Router here
+app.use(cors());
+app.use(morgan("dev"));
+app.use("/api", apiRouter);
+
+app.use((req, res, next) => {
+  console.log("<____Body Logger START____>");
+  console.log(req.body);
+  console.log("<_____Body Logger END_____>");
+
+  next();
+});
 
 module.exports = app;
