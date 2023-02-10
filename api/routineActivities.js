8760bd0297039b6a6876e024550ac663d1a7dbee
Zachary Collie <zachc4@vt.edu>
2023-02-10 01:25:37 -0500

    Routine Activities Delete Done

--- api/routineActivities.js
+++ api/routineActivities.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const routineActivitiesRouter = express.Router();
-const { destroyRoutineActivity } = require("../db/routine_activities");
+const {
+  destroyRoutineActivity,
+  getRoutineActivityById,
+} = require("../db/routine_activities");
+const { getAllRoutines } = require("../db/routines");
 // PATCH /api/routine_activities/:routineActivityId
 
 // DELETE /api/routine_activities/:routineActivityId
@@ -9,10 +13,31 @@ routineActivitiesRouter.delete(
   async (req, res, next) => {
     const { routineActivityId } = req.params;
 
-    try {
-      await destroyRoutineActivity(routineActivityId);
-    } catch (e) {
-      next(e);
+    if (req.user) {
+      try {
+        const allRoutines = await getAllRoutines();
+        const routineActivity = await getRoutineActivityById(routineActivityId);
+
+        await allRoutines.map((routine) => {
+          routine.activities.filter((activity) => {
+            if (req.user.id == routine.creatorId) {
+              if (routineActivity.id === activity.id) {
+                res.send(routineActivity);
+                destroyRoutineActivity(activity.id);
+              }
+            } else {
+              res.status(403);
+              next({
+                error: "No authentication",
+                name: "No authentication",
+                message: `User ${req.user.username} is not allowed to delete ${routine.name}`,
+              });
+            }
+          });
+        });
+      } catch (e) {
+        next(e);
+      }
     }
   }
 );
