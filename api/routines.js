64dad0785369ec74655b4d7dd3927283ec45c5f1
Zachary Collie <zachc4@vt.edu>
2023-02-10 14:57:23 -0500

    Started Routines API, 2/5 done.

--- api/routines.js
+++ api/routines.js
@@ -1,9 +1,42 @@
 const express = require("express");
 const routinesRouter = express.Router();
+const { getAllRoutines, createRoutine } = require("../db");
+const { createActivity } = require("../db/activities");
 
 // GET /api/routines
-
+routinesRouter.get("/", async (req, res, next) => {
+  try {
+    const routines = await getAllRoutines();
+    res.send(routines);
+  } catch (e) {
+    next(e);
+  }
+});
 // POST /api/routines
+// router.post("/", async (req, res, next) => {
+//
+// })
+
+routinesRouter.post("/", async (req, res, next) => {
+  const { isPublic, name, goal } = req.body;
+  try {
+    if (req.user.id) {
+      const createARoutine = await createRoutine({
+        creatorId: req.user.id,
+        isPublic,
+        name,
+        goal,
+      });
+      res.send(createARoutine);
+    }
+  } catch (e) {
+    next({
+      error: `You must be logged in to perform this action`,
+      name: "Login Error",
+      message: `You must be logged in to perform this action`,
+    });
+  }
+});
 
 // PATCH /api/routines/:routineId
 
