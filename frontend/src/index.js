3920bdc6971ec5239eaa6cde16d0dd95eacfef9b
Zachary Collie <zachc4@vt.edu>
2023-02-15 00:13:16 -0500

    FrontEnd Start, Login started

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,46 @@
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { HashRouter } from "react-router-dom";
+import {
+  colors,
+  CssBaseline,
+  ThemeProvider,
+  Typography,
+  Container,
+  createTheme,
+  Box,
+  SvgIcon,
+  Link,
+  Button,
+  TextField,
+} from "@mui/material";
+
+import App from "./App";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#556cd6",
+    },
+    secondary: {
+      main: "#19857b",
+    },
+    error: {
+      main: colors.red.A400,
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <ThemeProvider theme={theme}>
+    {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+    <CssBaseline />
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </ThemeProvider>
+);
