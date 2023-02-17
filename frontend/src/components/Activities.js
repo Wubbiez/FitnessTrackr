5ea4b15fd7125387fe09f3db0d9e469544b6c2a1
Kade Salerno <113953505+ksal44@users.noreply.github.com>
2023-02-17 14:05:46 -0600

    activites work

--- frontend/src/components/Activities.js
+++ frontend/src/components/Activities.js
@@ -1,38 +1,57 @@
-// import { useEffect, useState } from "react";
-// import styles from "./Activities.module.css";
-// import React from "react";
-// import { getAllActivities } from "../db/activities";
-// // import { useHistory } from 'react-router-dom';
-// //needs work
-//
-// const ActivitiesList = (props) => {
-//   const { token } = props;
-//   const [activities, setActivities] = useState([]);
-//   const [activitiesLoaded, setPostsLoaded] = useState([]);
-//   //   const history = useHistory()
-//
-//   useEffect(() => {
-//     try {
-//       getAllActivities(token).then((r) => setActivities(r.activities));
-//     } catch (e) {
-//       console.error(e);
-//     }
-//     setPostsLoaded(true);
-//   }, [token, setActivities, activitiesLoaded]);
-//
-//   return (
-//     <>
-//       <h1 className={styles.title}>Activities</h1>
-//       <div className={styles.routines}>
-//         {activities.map((activity) => (
-//           <div key={activity.id} className={styles.title}>
-//             <h3>{activity.name}</h3>
-//             <div>{activity.description}</div>
-//           </div>
-//         ))}
-//       </div>
-//     </>
-//   );
-// };
-//
-// export default ActivitiesList;
+import { useEffect, useState } from "react";
+import styles from "./Activities.module.css";
+import React from "react";
+import { CreateActivity } from "./createActivity"
+// import { useHistory } from 'react-router-dom';
+//needs work
+
+const ActivitiesList = (props) => {
+  const { token } = props
+  const [activities, setActivities] = useState([]);
+  //   const history = useHistory()
+
+  useEffect(() => {
+    const getActivities = async () => {
+      const response = await fetch(
+        `http://fitnesstrac-kr.herokuapp.com/api/activities`
+      );
+      const data = await response.json();
+      setActivities(data);
+      console.log(data);
+    };
+    getActivities();
+  }, []);
+  if (token) {
+    return (
+      <>
+        <button>create a new activity</button>
+        <h1 className={styles.title}>Activities</h1>
+        <div className={styles.routines}>
+          {activities.map((activity) => (
+            <div key={activity.id} className={styles.title}>
+              <h3>{activity.name}</h3>
+              <div>{activity.description}</div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <h1 className={styles.title}>Activities</h1>
+        <div className={styles.routines}>
+          {activities.map((activity) => (
+            <div key={activity.id} className={styles.title}>
+              <h3>{activity.name}</h3>
+              <div>{activity.description}</div>
+            </div>
+          ))}
+        </div>
+      </>
+    );
+
+  }
+}
+
+export default ActivitiesList
---
+++ frontend/src/components/createActivity.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+
+
+async function CreateActivity(props) {
+    const { token, name, description, setName, setDescription } = props;
+
+
+    return (
+        <form
+            onSubmit={
+                async (event) => {
+                    event.preventDefault();
+
+                    try {
+                        const response = await fetch('http://fitnesstrac-kr.herokuapp.com/api/activities',
+                            {
+                                method: "POST",
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                    'Authorization': `Bearer ${token}`
+                                },
+                                body: JSON.stringify({
+                                    activity: {
+                                        name,
+                                        description,
+                                    }
+                                })
+                            }).then(response => response.json())
+                            .then(result => {
+                                console.log(result)
+                            });
+
+                        console.log(response)
+
+
+
+                    } catch (err) {
+                        console.log('failed to make an Activity');
+                        console.error(err)
+                    }
+
+                }
+            }
+        >
+            <label>
+                Name
+                <input
+                    onChange={setName}
+                    value={name}
+                />
+            </label>
+            <label>
+                Description
+                <input
+                    onChange={setDescription}
+                    value={description}
+                />
+            </label>
+            <button>Submit Activity</button>
+
+        </form>
+    )
+
+}
+
+export default CreateActivity
